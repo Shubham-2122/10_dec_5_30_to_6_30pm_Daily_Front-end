e008b95cfacd285e731bc6dfa50922915aa48a41
Shubham-2122 <shubhamjadav.tops@gmail.com>
2025-04-15 18:39:59 +0530

    react

--- React/demo/src/App.js
+++ React/demo/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hello Header data</h1>
-    </div>
-  );
+function App(){
+    return(
+       <div>
+         <h1>Hell this App data</h1>
+         <h2>asdhkuh</h2>
+       </div>
+    )
 }
 
 export default App;
---
+++ React/reactvite/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>Hello this React app vite</h1>
+    </>
+  )
+}
+
+export default App
