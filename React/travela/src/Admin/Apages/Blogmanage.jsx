f4d9654f47c823ba610b6e40afe421a2c4cc6e67
Shubham-2122 <shubhamjadav.tops@gmail.com>
2025-06-18 18:47:31 +0530

    project

--- React/travela/src/Admin/Apages/Blogmanage.jsx
+++ React/travela/src/Admin/Apages/Blogmanage.jsx
@@ -16,12 +16,37 @@ function Blogmanage() {
         setblogs(res.data)
     }
 
+    const [blogdata, setblogdata] = useState({
+        id: "",
+        name: "",
+        posted: "",
+        desc: "",
+        date: "",
+        img: ""
+    })
+
+
+    // view data one
+    const getdata = async (id) => {
+        const res = await axios.get(`http://localhost:3000/blogs/${id}`)
+        console.log(res.data)
+        setblogdata(res.data)
+    }
+
+
+    // delete data
+    const removedata = async (id) => {
+        const res = await axios.delete(`http://localhost:3000/blogs/${id}`)
+        console.log(res.data)
+        fetchdata()
+    }
+
     return (
         <div>
             <AHeader title="Blog manage" name="Blogs" />
-           
+
             <div classname="container">
-                 <h1 className='text-center'>hello this Blog Page</h1>
+                <h1 className='text-center'>hello this Blog Page</h1>
                 <table className="table">
                     <thead>
                         <tr className='text-center'>
@@ -36,7 +61,7 @@ function Blogmanage() {
                     <tbody>
                         {
                             // console.log(blogs)
-                            blogs && blogs.map((data,index) => {
+                            blogs && blogs.map((data, index) => {
                                 console.log(data)
                                 return (
                                     <tr className='text-center' key={index}>
@@ -46,9 +71,9 @@ function Blogmanage() {
                                         <td>{data.desc}</td>
                                         <td>{data.date}</td>
                                         <td>
-                                            <button className='btn btn-primary'>View</button>
+                                            <button className='btn btn-primary' data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={() => getdata(data.id)}>View</button>
                                             <button className='btn btn-success mx-2'>Edit</button>
-                                            <button className='btn btn-danger'>Delete</button>
+                                            <button className='btn btn-danger' onClick={() => removedata(data.id)}>Delete</button>
                                         </td>
                                     </tr>
                                 )
@@ -58,6 +83,45 @@ function Blogmanage() {
 
                     </tbody>
                 </table>
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                     <div className="col-12">
+                                            <div className="blog-item">
+                                                <div className="blog-img">
+                                                    <div className="blog-img-inner">
+                                                        <img className="img-fluid w-100 rounded-top" style={{height:"200px"}} src={blogdata.img} alt="Image" />
+                                                        <div className="blog-icon">
+                                                            <a href="#" className="my-auto"><i className="fas fa-link fa-2x text-white" /></a>
+                                                        </div>
+                                                    </div>
+                                                    <div className="blog-info d-flex align-items-center border border-start-0 border-end-0">
+                                                        <small className="flex-fill text-center border-end py-2"><i className="fa fa-calendar-alt text-primary me-2" />{blogdata.date}</small>
+                                                        <a href="#" className="btn-hover flex-fill text-center text-white border-end py-2"><i className="fa fa-thumbs-up text-primary me-2" />1.7K</a>
+                                                        <a href="#" className="btn-hover flex-fill text-center text-white py-2"><i className="fa fa-comments text-primary me-2" />1K</a>
+                                                    </div>
+                                                </div>
+                                                <div className="blog-content border border-top-0 rounded-bottom p-4">
+                                                    <p className="mb-3">{blogdata.posted}</p>
+                                                    <a href="#" className="h4">{blogdata.name}</a>
+                                                    <p className="my-3">{blogdata.desc}</p>
+                                                    <a href="#" className="btn btn-primary rounded-pill py-2 px-4">Read More</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </div>
--- React/travela/src/Admin/Apages/Guidsmange.jsx
+++ React/travela/src/Admin/Apages/Guidsmange.jsx
@@ -16,6 +16,27 @@ function Guidsmange() {
         setguide(res.data)
     }
 
+     // delete data
+    const removedata = async (id) => {
+        const res = await axios.delete(`http://localhost:3000/guies/${id}`)
+        console.log(res.data)
+        fetchdata()
+    }
+
+       const [blogdata, setblogdata] = useState({
+            id: "",
+            name: "",
+            Designation: "",
+            img: ""
+        })
+
+     // view data one
+    const getdata = async (id) => {
+        const res = await axios.get(`http://localhost:3000/blogs/${id}`)
+        console.log(res.data)
+        setblogdata(res.data)
+    }
+
     return (
         <div>
             <AHeader title="Guide Manage" name="Guide" />
@@ -44,9 +65,9 @@ function Guidsmange() {
                                             <img src={data.img} style={{height:"50px",width:"50px"}} alt="" />
                                         </td>
                                         <td>
-                                            <button className='btn btn-primary'>View</button>
+                                            <button className='btn btn-primary' data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={()=>getdata(data.id)}>View</button>
                                             <button className='btn btn-success mx-2'>Edit</button>
-                                            <button className='btn btn-danger'>Delete</button>
+                                            <button className='btn btn-danger' onClick={()=>removedata(data.id)}>Delete</button>
                                         </td>
                                     </tr>
                                 )
@@ -56,6 +77,43 @@ function Guidsmange() {
 
                     </tbody>
                 </table>
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                    <div className="col-12">
+                                            <div className="guide-item">
+                                                <div className="guide-img">
+                                                    <div className="guide-img-efects">
+                                                        <img src={blogdata.img} style={{height:"250px"}} className="img-fluid w-100 rounded-top" alt="Image" />
+                                                    </div>
+                                                    <div className="guide-icon rounded-pill p-2">
+                                                        <a className="btn btn-square btn-primary rounded-circle mx-1" href><i className="fab fa-facebook-f" /></a>
+                                                        <a className="btn btn-square btn-primary rounded-circle mx-1" href><i className="fab fa-twitter" /></a>
+                                                        <a className="btn btn-square btn-primary rounded-circle mx-1" href><i className="fab fa-instagram" /></a>
+                                                        <a className="btn btn-square btn-primary rounded-circle mx-1" href><i className="fab fa-linkedin-in" /></a>
+                                                    </div>
+                                                </div>
+                                                <div className="guide-title text-center rounded-bottom p-4">
+                                                    <div className="guide-title-inner">
+                                                        <h4 className="mt-3">{blogdata.name}</h4>
+                                                        <p className="mb-0">{blogdata.Designation}</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
